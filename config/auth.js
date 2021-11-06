86357b7aee0c2272310a457213329eff80004a26
Deepak <deepak.singh@thethingscloud.com>
2021-11-06 14:25:03 +0530

    changes

--- config/auth.js
+++ config/auth.js
@@ -7,51 +7,25 @@ exports.createToken = function(user) {
 		user_id: user.user_id,
 		name: user.name
 	};
-	payload = JSON.stringify(payload);
-
-	var objJson = {};
-	objJson.payload = payload;
-	objJson.signature = jwt.sign(
+	var token = jwt.sign(
 		{payload: payload}, 
 		nconf.get('secret-key'),
-		{expiresIn: 60*60}
+		{expiresIn: 60*60},
 	);
-
-	var token = encodeBase64(objJson)
 	return token;
 };
 
 
 exports.authenticate = function(req, res, next) {
-	var token = decodeBase64(req.headers.authorization);
+	var token = req.headers.authorization;
 	try {
-		var decoded = jwt.verify(token.signature, nconf.get('secret-key'));
+		var decoded = jwt.verify(token, nconf.get('secret-key'));
 	} catch (err) {
 		return next({
 			status: 401,
 			message: "Authorisation token is invalid or expired!"
 		})
 	}
-	if(decoded.payload != token.payload) {
-		return next({
-			status: 401,
-			message: "Authorisation token is invalid or expired!"
-		})
-	}
-	res.locals.user_info = token.payload;
+	res.locals.user_info = decoded.payload;
 	next();
-};
-
-
-var encodeBase64 = function(objJson) {
-	var objJsonStr = JSON.stringify(objJson);
-	var encoded = Buffer.from(objJsonStr).toString("base64");
-	return encoded;
-};
-
-
-var decodeBase64 = function(str) {
-	var obj = Buffer.from(str, 'base64').toString('ascii');
-	obj = JSON.parse(obj);
-	return obj;
 };
